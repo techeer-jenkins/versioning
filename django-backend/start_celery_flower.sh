a3d4505db3b279952094dfd81f037afcf5d2a763
Ryan <leehahnwoong@gmail.com>
2024-07-12 00:48:38 -0700

    remove: backends

--- django-backend/start_celery_flower.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-celery -A todo_django worker --loglevel=info --concurrency=1 &
-
-celery -A todo_django flower --port=5555 --basic_auth=guest:guest
--- django-backend/todo_django/todo/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-import time
-
-@shared_task
-def add_with_delay(x, y):
-    time.sleep(2)
-    return x + y
